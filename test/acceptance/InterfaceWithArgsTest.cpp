e9b5acfe8f0cde8b2bfc47aa63d034f9675ca9f5
Rafal Przywarski <przywars@przywars-Latitude-E5420>
2012-07-24 15:50:45 +0200

    methods with arguments

--- test/acceptance/InterfaceWithArgsTest.cpp
+++ test/acceptance/InterfaceWithArgsTest.cpp
@@ -1,9 +1,23 @@
 #include "InterfaceWithArgsMock.hpp"
 #include <gtest/gtest.h>
 
+namespace
+{
+
+const char *ccf()
+{
+    return 0;
+}
+
+}
+
 TEST(InterfaceWithArgsMockTest, methods)
 {
     InterfaceWithArgsMock mock;
     EXPECT_CALL(mock, m1(7));
     static_cast<InterfaceWithArgs&>(mock).m1(7);
+    EXPECT_CALL(mock, m2(true, 3.2));
+    static_cast<InterfaceWithArgs&>(mock).m2(true, 3.2);
+    EXPECT_CALL(mock, m3('a', "b", ccf));
+    static_cast<InterfaceWithArgs&>(mock).m3('a', "b", ccf);
 }
